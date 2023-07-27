e6de00c5d4dc75c4a7eb8168635584a403bbc88e
nadil11 <nadilnidhal111@gmail.com>
2023-07-27 11:24:23 +0530

    27/7/2023

---
+++ src/Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Error() {
+  return (
+    <>
+      <div>
+        <p>Page not found!</p>
+      </div>
+    </>
+  );
+}
+export default Error;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App1 from './App1';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Accounts from "./Accounts";
+import Password from "./Password";
+import Security from "./Security";
+import Application from "./Application";
+import Notification from "./Notification";
+import Error from "./Error";
+import "./Router.css";
 
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <App1/>
-
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="accounts" element={<Accounts />}></Route>
+          <Route path="password" element={<Password />}></Route>
+          <Route path="security" element={<Security />}></Route>
+          <Route path="application" element={<Application />}></Route>
+          <Route path="notification" element={<Notification />}></Route>
+        </Route>
+        <Route path="*" element={<Error/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  </>
 );
